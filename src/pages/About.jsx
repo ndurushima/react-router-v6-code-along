40a05e481dbbef339946c6b644bb3655ff9e11bf
ndurushima <ndu1994@gmail.com>
2025-07-17 14:45:20 -0500

    create login and about pages with routing

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+
+
+function About() {
+    return (
+        <>
+            <header>
+                {/* Save space for NavBar */}
+            </header>
+            <main>
+                <h1>This is my first component!</h1>
+            </main>
+        </>
+    )
+}
+
+export default About
---
+++ src/pages/Login.jsx
@@ -0,0 +1,29 @@
+
+
+function Login() {
+    return (
+        <>
+            <header>
+                {/* Save space for NavBar */}
+            </header>
+            <main>
+                <h1>Login</h1>
+                <form>
+                    <div>
+                        <label htmlFor="username" >Username: </label>
+                        <input 
+                            id="username"
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                        />
+                    </div>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </form>
+            </main>
+        </>
+    )
+}
+
+export default Login
